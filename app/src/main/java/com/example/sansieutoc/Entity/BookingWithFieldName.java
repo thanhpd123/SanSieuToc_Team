53e68328923d46de9fb92963886bc2b079e7bf6a
dungtthe171658 <dungtthe171658@fpt.edu.vn>
2025-07-23 09:14:58 +0700

    Notification

---
+++ app/src/main/java/com/example/sansieutoc/Entity/BookingWithFieldName.java
@@ -0,0 +1,11 @@
+package com.example.sansieutoc.Entity;
+
+public class BookingWithFieldName {
+    public int id;
+    public String fieldName;
+    public String date;
+    public String startTime;
+    public String endTime;
+    public String status;
+    public String createdAt;
+}
